87f210d4bf638bd79e36adab3249ef626911596a
vinicius <vinicius.marinho@lipperhub.com>
2019-02-25 13:53:09 -0300

    Adding Bottom Bar creator.

---
+++ dist/index.js
@@ -0,0 +1,34 @@
+
+/* eslint-disable import/no-commonjs */
+
+module.exports = {
+  /**
+   * Navigators
+   */
+  get createBottomTabNavigator() {
+    return require('./navigators/createBottomTabNavigator').default;
+  },
+  get createMaterialTopTabNavigator() {
+    return require('./navigators/createMaterialTopTabNavigator').default;
+  },
+  get createMaterialBottomTabNavigator() {
+    return require('./navigators/createMaterialBottomTabNavigator').default;
+  },
+
+  /**
+   * Views
+   */
+  get BottomTabBar() {
+    return require('./views/BottomTabBar').default;
+  },
+  get MaterialTopTabBar() {
+    return require('./views/MaterialTopTabBar').default;
+  },
+
+  /**
+   * Utils
+   */
+  get createTabNavigator() {
+    return require('./utils/createTabNavigator').default;
+  }
+};
